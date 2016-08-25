9271e9ce5008405bbc2511971b86119547cb2475
Gregory Smith <crashingwaves@fea.st>
2016-08-25 11:35:47 -0600

    App working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,20 @@ import $ from 'jquery';
 import handlebars from 'handlebars';
 
 $(document).ready(() => {
-    const quoteTemplate = handlebars.compile($(''))
-    getData().then(data => {
-        $('#quote').html(quoteTemplate(data));
-    }).catch(error => {
+    const quoteTemplate = handlebars.compile($('#quote-template').html()),
+        errorTemplate = handlebars.compile($('#error-template').html());
+    $('#quote-button').on('click', getData);
 
-    });
+    function getData(event) {
+        if(event) {
+            event.preventDefault();
+        }
+
+        $.get('http://localhost:9000/quote').then(data => {
+            $('#quote').html(quoteTemplate(data));
+        }).catch(error => {
+            $('#quote').html(errorTemplate(error));
+        });
+}
 });
 
-function getData() {
-    return $.get('http://localhost:9000/quote');
-}
